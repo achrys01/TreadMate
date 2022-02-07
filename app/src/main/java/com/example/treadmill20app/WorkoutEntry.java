05e6d809e2d23ad273abf2c5677edabc1fa72712
achrys01 <91139923+achrys01@users.noreply.github.com>
2022-02-07 11:12:56 +0100

    Placeholders

---
+++ app/src/main/java/com/example/treadmill20app/WorkoutEntry.java
@@ -0,0 +1,24 @@
+package com.example.treadmill20app;
+
+public class WorkoutEntry {
+    private float dur;
+    private float durStep;
+    private float speed;
+    private float speedStep;
+    private float incl;
+    private float inclStep;
+
+    public void setDur(float durEntry){dur = durEntry;}
+    public void setSpeed(float speedEntry){speed = speedEntry;}
+    public void setIncl(float inclEntry){incl = inclEntry;}
+
+    public float getDur(){
+        return durStep = dur;
+    }
+    public float getSpeed(){
+        return speedStep = speed;
+    }
+    public float getIncl(){
+        return inclStep = incl;
+    }
+}
---
+++ app/src/main/java/com/example/treadmill20app/WorkoutObject.java
@@ -0,0 +1,4 @@
+package com.example.treadmill20app;
+
+public class WorkoutObject {
+}
