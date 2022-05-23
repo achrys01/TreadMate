3505176e020ef434d62f3cfd5f7c69f444aacccf
achrys01 <91139923+achrys01@users.noreply.github.com>
2022-05-23 17:06:51 +0200

    Revert "Merge remote-tracking branch 'origin/treadmill_service' into workout_design"
    This reverts commit 742779a47766e1ff2bc206e06655a6faf5a6b075, reversing
    changes made to e84116934ad5dea2652d056685330051bb891dc1.

--- app/src/main/java/com/example/treadmill20app/BtServices/GattActions.java
+++ app/src/main/java/com/example/treadmill20app/BtServices/GattActions.java
@@ -13,68 +13,22 @@ public class GattActions {
     public final static String ACTION_GATT_HEART_RATE_EVENTS =
             "com.example.treadmill20app.BtServices.ACTION_GATT_HEART_RATE_EVENTS";
 
-    //The action corresponding to ftms events from BleFtmsService.
-    public final static String ACTION_GATT_FTMS_EVENTS =
-            "com.example.treadmill20app.BtServices.ACTION_GATT_Treadmill_EVENTS";
-
-    //A flag for hr event info in intents (via intent.putExtra)
-    public final static String HR_EVENT =
-            "com.example.treadmill20app.BtServices.HR_EVENT";
-
-    //A flag for hr event info in intents (via intent.putExtra)
-    public final static String FTMS_EVENT =
-            "com.example.treadmill20app.BtServices.FTMS_EVENT";
+    //A flag for event info in intents (via intent.putExtra)
+    public final static String EVENT =
+            "com.example.treadmill20app.BtServices.EVENT";
 
     //A flag for heart rate data in intent (via intent.putExtra)
     public final static String HEART_RATE_DATA =
             "com.example.treadmill20app.BtServices.HEART_RATE_DATA";
 
-    //Flags for treadmill data in intent (via intent.putExtra)
-    public final static String TOTAL_DISTANCE_DATA =
-            "com.example.treadmill20app.BtServices.TOTAL_DISTANCE_DATA";
-    public final static String INSTANT_SPEED_DATA =
-            "com.example.treadmill20app.BtServices.INSTANT_SPEED_DATA";
-    public final static String INCLINATION_DATA =
-            "com.example.treadmill20app.BtServices.INCLINATION_DATA";
-
-    //Flags for supported speed data in intent (via intent.putExtra)
-    public final static String MIN_SPEED =
-            "com.example.treadmill20app.BtServices.MIN_SPEED";
-    public final static String MAX_SPEED =
-            "com.example.treadmill20app.BtServices.MAX_SPEED";
-    public final static String SPEED_INCREMENT =
-            "com.example.treadmill20app.BtServices.SPEED_INCREMENT";
-
-    //Flags for supported inclination data in intent (via intent.putExtra)
-    public final static String MIN_INCL =
-            "com.example.treadmill20app.BtServices.INCL_SPEED";
-    public final static String MAX_INCL =
-            "com.example.treadmill20app.BtServices.INCL_SPEED";
-    public final static String INCL_INCREMENT =
-            "com.example.treadmill20app.BtServices.INCL_INCREMENT";
-
-    //Flags for control characteristics in intent (via intent.putExtra)
-    public final static String CONTROL_TYPE =
-            "com.example.treadmill20app.BtServices.CONTROL_TYPE";
-    public final static String CONTROL_VALUE =
-            "com.example.treadmill20app.BtServices.CONTROL_VALUE";
-    public final static String COMMAND_CHAR =
-            "com.example.treadmill20app.BtServices.COMMAND_CHAR";
-    //Flags for status characteristics in intent (via intent.putExtra)
-    public final static String STATUS_TYPE =
-            "com.example.treadmill20app.BtServices.STATUS_TYPE";
-    public final static String STATUS_VALUE =
-            "com.example.treadmill20app.BtServices.STATUS_VALUE";
-
-    //Events corresponding to HR Gatt status/events
-    public enum HR_Event {
+    //Events corresponding to Gatt status/events
+    public enum Event {
         GATT_CONNECTED("Connected"),
-        GATT_CONNECTING("Connecting"),
         GATT_DISCONNECTED("Disconnected"),
         GATT_SERVICES_DISCOVERED("Services discovered"),
         HEART_RATE_SERVICE_DISCOVERED("Heart rate service"),
         HEART_RATE_SERVICE_NOT_AVAILABLE("Heart rate service unavailable"),
-        HR_DATA_AVAILABLE("HR Data available");
+        DATA_AVAILABLE("Data available");
 
         @Override
         public String toString() {
@@ -83,35 +37,7 @@ public class GattActions {
 
         private final String text;
 
-        private HR_Event(String text) {
-            this.text = text;
-        }
-
-    }
-
-    //Events corresponding to FTMS Gatt status/events
-    public enum FTMS_Event {
-        GATT_CONNECTED("Connected"),
-        GATT_CONNECTING("Connecting"),
-        GATT_DISCONNECTED("Disconnected"),
-        GATT_SERVICES_DISCOVERED("Services discovered"),
-        FTMS_SERVICE_DISCOVERED("FTMS service"),
-        FTMS_SERVICE_NOT_AVAILABLE("FTMS service unavailable"),
-        FTMS_DATA_AVAILABLE("Ftms Data available"),
-        SUPPORTED_SPEED("Supported speed received"),
-        SUPPORTED_INCLINATION("Supported inclination received"),
-        FTMS_CONTROL("FTMS control changed"),
-        FTMS_COMMAND("FTMS command sent"),
-        FTMS_STATUS("FTMS status changed");
-
-        @Override
-        public String toString() {
-            return text;
-        }
-
-        private final String text;
-
-        private FTMS_Event(String text) {
+        private Event(String text) {
             this.text = text;
         }
 
